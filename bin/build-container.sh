270dbf21974165755647c3fac08e5ec6200340da
Yuang Deng <lnsydengyuang@gmail.com>
2022-07-04 10:52:23 +0800

    remove install autogoluon locally
    it not necessary for all users

--- bin/build-container.sh
+++ bin/build-container.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 set -e
 
+echo "Building image..."
+docker build \
+    -t $REGISTRY/ts-bench:latest \
+    -f Dockerfile .
+
 echo "Fetching credentials from AWS..."
 AWS_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
 AWS_REGION=$(aws configure get region)
 ECR_PASSWORD=$(aws ecr get-login-password)
 REGISTRY=$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com
 
-DOCKERFILE_PATH="Dockerfile"
-if [ "$1" = "local" ];
-then
-    echo "Build docker image with local autogluon"
-    DOCKERFILE_PATH="Dockerfile_local"
-else
-    echo "Build docker image with remote autogluon"     
-fi
-
-echo "Building image..."
-docker build \
-    -t $REGISTRY/tsbench-autogluon:jul1_1 \
-    -f $DOCKERFILE_PATH . 
-
 echo "Logging in to ECR..."
 echo $ECR_PASSWORD | \
     docker login --username AWS --password-stdin $REGISTRY
 
 echo "Pushing image..."
-docker push $REGISTRY/tsbench-autogluon:jul1_1
+docker push $REGISTRY/tsbench:latest
